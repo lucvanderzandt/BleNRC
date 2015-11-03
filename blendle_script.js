ea93fd9ab23a53c1ae84cd2459babd2bc53c7470
Luc van der Zandt <lucvanderzandt@kpnplanet.nl>
2015-11-03 22:16:03 +0100

    blendle_script.js v0.1 - added comments

--- blendle_script.js
+++ blendle_script.js
@@ -1,3 +1,4 @@
-if(document.referrer.indexOf("nrc.nl/handelsblad/") > -1 || document.referrer.indexOf("nrc.nl/next/") > -1 && document.documentElement.outerHTML.indexOf("Forbidden") > -1){
-    history.back();
+// If 403-status (forbidden), go to normal NRC page with parameter noscript (don't execute nrc_script.js) 
+if((document.referrer.indexOf("nrc.nl/handelsblad/") > -1 || document.referrer.indexOf("nrc.nl/next/") > -1) && document.documentElement.outerHTML.indexOf("Forbidden") > -1){
+    window.location.replace(document.referrer + "?noscript");
 }
