619eea1113048df772d3f2ff7becc7bc510b95d1
Luc van der Zandt <lucvanderzandt@kpnplanet.nl>
2015-11-04 00:17:16 +0100

    blendle_script.js v0.1.1 - now checking for complete html of forbidden-page

--- blendle_script.js
+++ blendle_script.js
@@ -1,4 +1,4 @@
-// If 403-status (forbidden), go to normal NRC page with parameter noscript (don't execute nrc_script.js) 
-if((document.referrer.indexOf("nrc.nl/handelsblad/") > -1 || document.referrer.indexOf("nrc.nl/next/") > -1) && document.documentElement.outerHTML.indexOf("Forbidden") > -1){
+// If 403-page (forbidden), go to normal NRC page with parameter noscript (don't execute nrc_script.js) 
+if((document.referrer.indexOf("nrc.nl/handelsblad/") > -1 || document.referrer.indexOf("nrc.nl/next/") > -1) && document.documentElement.outerHTML.indexOf('<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">Forbidden</pre></body></html>') > -1){
     window.location.replace(document.referrer + "?noscript");
 }
