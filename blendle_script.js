83746d78a2be165b351a8fac27cb5425f1e00374
Luc van der Zandt <lucvanderzandt@kpnplanet.nl>
2015-11-06 14:12:56 +0100

    blendle_script.js v0.1.2 - hide Blendle page until loaded

--- blendle_script.js
+++ blendle_script.js
@@ -1,4 +1,10 @@
+// Hide page until checked if valid
+document.body.style.display = 'none';
+
 // If 403-page (forbidden), go to normal NRC page with parameter noscript (don't execute nrc_script.js) 
-if((document.referrer.indexOf("nrc.nl/handelsblad/") > -1 || document.referrer.indexOf("nrc.nl/next/") > -1) && document.documentElement.outerHTML.indexOf('<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">Forbidden</pre></body></html>') > -1){
+if((document.referrer.indexOf("nrc.nl/handelsblad/") > -1 || document.referrer.indexOf("nrc.nl/next/") > -1) && document.documentElement.outerHTML.indexOf('<pre style="word-wrap: break-word; white-space: pre-wrap;">Forbidden</pre>') > -1){
     window.location.replace(document.referrer + "?noscript");
+}else{
+    // Show page
+    document.body.style.display = 'block';
 }
